1a0464bdaa267b3f4046ef821f863f3e97eddaa1
Starxy <a136009394@163.com>
2018-11-23 22:14:11 +0800

    less code than 001_v2

--- 349IntersectionofTwoArrays_v1.java
+++ 349IntersectionofTwoArrays_v1.java
@@ -18,9 +18,7 @@ class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         Set<Integer> set = new HashSet<>();
         Set<Integer> intersect = new HashSet<>();
- /*       for (int i = 0; i < nums1.length; i++) {
-            set.add(nums1[i]);
-        }*/
+
         for (int i : nums1)
             set.add(i);
         
