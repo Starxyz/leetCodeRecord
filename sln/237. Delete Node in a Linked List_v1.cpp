761e850db790338d7c283261d3a81b7939a11b76
Starxy <a136009394@163.com>
2018-11-27 15:33:56 +0800

    auto comment

---
+++ sln/237. Delete Node in a Linked List_v1.cpp
@@ -0,0 +1,53 @@
+/*237. Delete Node in a Linked List
+Write a function to delete a node in a singly linked list, given only access to that node.
+
+Given linked list -- head = [4,5,1,9], which looks like following:
+
+    4 -> 5 -> 1 -> 9
+Example 1:
+
+Input: head = [4,5,1,9], node = 5
+Output: [4,1,9]
+Explanation: You are given the second node with value 5, the linked list
+             should become 4 -> 1 -> 9 after calling your function.
+Example 2:
+
+Input: head = [4,5,1,9], node = 1
+Output: [4,5,9]
+Explanation: You are given the third node with value 1, the linked list
+             should become 4 -> 5 -> 9 after calling your function.
+Note:
+
+The linked list will have at least two elements.
+All of the nodes' values will be unique.
+it will always be a valid node of the linked list.
+Do not return anything from your function.
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        if (node == NULL) {
+            return;
+        }
+        if (node->next == NULL) {  //tail
+            delete node;
+            node = NULL;
+            return;
+        }
+        
+        node->val = node->next->val;
+        ListNode* delNode = node->next;
+        node->next = delNode->next;
+        delete delNode;
+        
+        return;
+    }
+};
