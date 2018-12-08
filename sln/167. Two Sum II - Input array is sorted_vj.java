8234d508226d4065cbb7bd72cf692fe0a9de45db
Starxy <a136009394@163.com>
2018-12-08 14:57:39 +0800

    edit comment

--- sln/167. Two Sum II - Input array is sorted_vj.java
+++ sln/167. Two Sum II - Input array is sorted_vj.java
@@ -13,6 +13,7 @@ Input: numbers = [2,7,11,15], target = 9
 Output: [1,2]
 Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
 */
+// Since there will be exactly one solution for each array, so a while loop as below will do.
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int r = numbers.length - 1;
