d9938c52ea995a9893703590330ce5cbfad695bc
Starxyz <a136009394@163.com>
2018-12-09 22:29:34 +0800

    589 solved:

---
+++ sln/589_N-aryTreePreorderTraversal.java
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<Integer> preorder(Node root) {
+        
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        
+        Stack<Node> stack = new Stack();
+        stack.add(root);
+        
+        while (!stack.empty()) {
+            root = stack.pop();
+            res.add(root.val);
+            for (int i = root.children.size() - 1; i >= 0; i--) {
+                stack.add(root.children.get(i));
+            }
+        }
+        
+        return res;
+    }
+}
+
+
