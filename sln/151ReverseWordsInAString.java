4131a190ec245a1c39b40d4965f28767539a767b
Starxyz <a136009394@163.com>
2018-12-10 22:35:58 +0800

    151 solved

---
+++ sln/151ReverseWordsInAString.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder();
+        String[] words = s.trim().split("\\s+");
+        
+        for (int i = words.length-1; i >= 0; i--) {
+            sb.append(words[i] + " ");
+        }
+        
+        return sb.toString().trim();
+    }
+}
