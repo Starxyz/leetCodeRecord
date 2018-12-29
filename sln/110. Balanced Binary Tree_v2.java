21c4428da486791fe5ff3f13c496d3dc4cc34356
Starxy <a136009394@163.com>
2018-12-29 23:42:04 +0800

    110 version2

---
+++ sln/110. Balanced Binary Tree_v2.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        
+        if (checkHeight(root) == -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    
+    private int checkHeight(TreeNode root) {
+        
+        if (root == null) {
+            return 0;
+        }
+        
+        int leftHeight = checkHeight(root.left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+        
+        int rightHeight = checkHeight(root.right);
+        if (rightHeight == -1) {
+            return -1;
+        }
+        
+        int heightDiff = leftHeight - rightHeight;
+        if (Math.abs(heightDiff) > 1) {
+            return -1;
+        }
+        else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+}
