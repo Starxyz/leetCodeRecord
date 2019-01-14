621eca68163d9db662123453081a246cb5bae435
Starxy <a136009394@163.com>
2019-01-14 22:27:11 +0800

    203 java version

---
+++ sln/203. Remove Linked List Elements_vj.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode cur = dummyHead;
+        
+        while (cur != null && cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+            }
+            else
+                cur = cur.next;
+        }
+        return dummyHead.next;
+    }
+}
