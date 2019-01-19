afbb724b751f8045aa621b116af6b22c60f0b04c
Starxy <a136009394@163.com>
2019-01-19 19:03:17 +0800

    2_3 solved

---
+++ crackingTheCodingInterview/2_3deleteNode_v1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Remove {
+    public boolean removeNode(ListNode pNode) {
+        // write code here
+        if (pNode == null || pNode.next == null)
+            return false;
+        
+        pNode.val = pNode.next.val;
+        pNode.next = pNode.next.next;
+        return true;
+    }
+}
