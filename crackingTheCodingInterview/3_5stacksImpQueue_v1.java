9f278dcdb53dcddf6d9010e8daca7efcf35ac784
Starxy <a136009394@163.com>
2019-01-23 21:50:03 +0800

    3_5 solved

---
+++ crackingTheCodingInterview/3_5stacksImpQueue_v1.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+public class Solution {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+    
+    public void push(int node) {
+        stack1.push(node);// the newest element
+    }
+    
+    public int pop() {
+        shiftStacks();
+        return stack2.pop();
+    }
+    
+    private void shiftStacks() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+    }
+}
