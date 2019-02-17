8e4ce829d1ed05208c3454b9b19227fd4bdf0355
Starxy <a136009394@163.com>
2019-02-17 22:50:23 +0800

    015 more readable

--- sln/15. 3Sum_v1.java
+++ sln/15. 3Sum_v1.java
@@ -2,23 +2,22 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> res = new LinkedList<>();
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
-                int lo = i + 1;
-                int hi = nums.length - 1;
+        
+        for (int i = 0; i < nums.length-2; i++) {
+            if (i == 0 || (i > 0) && nums[i] != nums[i-1]) {
+                int l = i + 1;
+                int r = nums.length - 1;// two pointer for 2-sum
                 int sum = 0 - nums[i];
-                // as two sum
-                while (lo < hi) {
-                    if (nums[lo] + nums[hi] == sum) {
-                        res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
-                        while (lo < hi && nums[lo] == nums[lo+1]) lo++;
-                        while (lo < hi && nums[hi] == nums[hi-1]) hi--;
-                        lo++;
-                        hi--;
-                    }
-                    else if(nums[lo] + nums[hi] < sum)
-                        lo++;
-                    else hi--;
+                while (l < r) {
+                    if (nums[l] + nums[r] == sum) {
+                        res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                        while (l < r && nums[l] == nums[l+1]) l++;
+                        while (l < r && nums[r] == nums[r-1]) r--;
+                        l++;
+                        r--;// 3-sum consume 3 elements
+                    } 
+                    else if (nums[l] + nums[r] < sum) l++;
+                    else r--;
                 }
             }
         }
