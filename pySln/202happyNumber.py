ae57e2cc6e0f5a3b44d4591bea0e64c990a5e3fc
Starxyz <a136009394@163.com>
2020-05-22 23:56:22 +0800

    add  202

---
+++ pySln/202happyNumber.py
@@ -0,0 +1,31 @@
+# Write an algorithm to determine if a number n is "happy".
+
+# A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits,
+#  and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. 
+# Those numbers for which this process ends in 1 are happy numbers.
+
+# Return True if n is a happy number, and False if not.
+
+# Example: 
+
+# Input: 19
+# Output: true
+# Explanation: 
+# 1 * 1 + 9 * 9 = 82
+# 8 * 8 + 2 * 2 = 68
+# 6 * 6 + 8 * 8 = 100
+# 1 * 1 + 0 = 1
+# True
+
+def isHappy(n: int) -> bool:
+    record = set()
+    while n != 1:
+        n = sum([int(i) ** 2 for i in str(n)])
+        if n in record:
+            return False
+        else:
+            record.add(n)
+
+    return True
+
+
