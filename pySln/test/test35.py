58c404bdffd7d15b945324edcd7d27d2f9fd856c
Starxyz <a136009394@163.com>
2020-07-18 13:34:40 +0800

    add sln

---
+++ pySln/test/test35.py
@@ -0,0 +1,25 @@
+from leetcode35 import Solution
+
+sln = Solution()
+
+
+def test_case0():
+    nums = [1, 3, 5, 6]
+    target = 5
+    assert 2 == sln.searchInsert(nums, target)
+
+def test_case1():
+    nums = [1,3,5,6]
+    target = 2
+    assert 1 == sln.searchInsert(nums, target)
+
+def test_case0():
+    nums = [1, 3, 5, 6]
+    target = 5
+    assert 2 == sln.searchInsert(nums, target)
+
+def test_case0():
+    nums = [1, 3, 5, 6]
+    target = 5
+    assert 2 == sln.searchInsert(nums, target)
+
