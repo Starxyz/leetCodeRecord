a07f9ba20453c0b8b235e66d2cf2223ebf903424
Starxyz <a136009394@163.com>
2020-10-02 12:14:00 +0800

    add leetcode 206 method 2

--- pySln/leetcode206.py
+++ pySln/leetcode206.py
@@ -4,20 +4,36 @@
 #         self.val = x
 #         self.next = None
 
+# class Solution:
+#     def reverseList(self, head: ListNode) -> ListNode:
+#         def _reverse(head):
+
+#             if not head or not head.next:
+#                 return head
+            
+#             cur = _reverse(head.next)   # 这一步比较容易忘记
+#             head.next.next = head
+#             head.next = None
+
+#             return cur
+
+
+#         return _reverse(head)
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        def _reverse(head):
+        prev = None
+        cur = head
 
-            if not head or not head.next:
-                return head
-            
-            cur = _reverse(head.next)   # 这一步比较容易忘记
-            head.next.next = head
-            head.next = None
-
-            return cur
-
-
-        return _reverse(head)
-
+        while cur:  # 左边很好记，就是next，cur， prev
+            cur.next, cur, prev = prev, cur.next, cur
+        
+        return prev
 
